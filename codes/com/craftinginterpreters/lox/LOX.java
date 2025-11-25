1e3b7fb0bf5cc1031cd7819441ba0ccec4e3ddc8
ciko23168 <ciko23168@g.nihon-u.ac.jp>
2025-11-25 12:34:48 +0900

    add Scanner.java add Token.java

--- codes/com/craftinginterpreters/lox/LOX.java
+++ codes/com/craftinginterpreters/lox/LOX.java
@@ -61,4 +61,3 @@ public class Lox {
     hadError = true;
   }
 }
-      
--- codes/com/craftinginterpreters/lox/TokenType.java
+++ codes/com/craftinginterpreters/lox/TokenType.java
@@ -1,21 +1,27 @@
 ﻿package com.craftinginterpreters.lox;
 
-public class TokenType {
-     // Single-character tokens.
-     LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-     COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+public enum TokenType {
+  // Single-character tokens.
 
-     // One or two character tokens.
-     BANG, BANG_EQUAL,
-     EQUAL, EQUAL_EQUAL,
-     GREATER, GREATER_EQUAL,
-     LESS, LESS_EQUAL,
-     // Literals.
-     IDENTIFIER, STRING,NUMBER,
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
-     // Keywords.
-      AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+  // One or two character tokens.
+
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+  LESS, LESS_EQUAL,
+
+  // Literals.
+
+  IDENTIFIER, STRING, NUMBER,
+
+  // Keywords.
+
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
   PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
 
   EOF
 }
+
